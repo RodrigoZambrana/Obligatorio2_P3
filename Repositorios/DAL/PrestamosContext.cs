89945446fd0229175be0ea10a8b55bb08c24ff4a
Gaboalien <abelendagabriel5@gmail.com>
2020-11-28 16:41:02 -0300

    Creación del context , y métodos en el controller de la web api

---
+++ Repositorios/DAL/PrestamosContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Dominio;
+
+namespace Repositorios.DAL
+{
+    public class PrestamosContext : DbContext
+    {
+        public DbSet<Proyecto> Proyectos { get; set; }
+        public DbSet<Cooperativo> Cooperativos { get; set; }
+        public DbSet<Personal> Personales { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Solicitante> Solicitantes { get; set; }
+
+        
+        public PrestamosContext() : base("con2020")
+        {
+
+        }
+
+    }
+}
