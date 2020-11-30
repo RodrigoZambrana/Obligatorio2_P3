883737ade779639d6121601759e30e5779738b04
Gaboalien <abelendagabriel5@gmail.com>
2020-11-30 17:45:34 -0300

    Funciona consumir datos de la web api, agregue proyectomodel.

--- Obligatorio/Controllers/ProyectoController.cs
+++ Obligatorio/Controllers/ProyectoController.cs
@@ -3,176 +3,204 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Dominio;
-using Repositorios;
+using Obligatorio.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
 
 namespace Obligatorio.Controllers
 {
     public class ProyectoController : Controller
     {
-        // GET: Proyecto
-        //public ActionResult Index()
+        HttpClient cliente = new HttpClient();
+        HttpResponseMessage response = new HttpResponseMessage();
+        Uri proyectoUri = null;
+
+        public ProyectoController()
+        {
+            cliente.BaseAddress = new Uri("http://localhost:31069");
+            proyectoUri = new Uri("http://localhost:52230/api/proyecto");
+            cliente.DefaultRequestHeaders.Accept.Clear();
+            cliente.DefaultRequestHeaders.Accept.Add(
+       new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        // GET: Productos
+        public ActionResult Index()
+        {
+            response = cliente.GetAsync(proyectoUri).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var proys = response.Content.ReadAsAsync<IEnumerable<Models.ProyectoModel>>().Result;
+                /*   var lista = prods.Select
+                       (p => new Models.ProductoModel { Nombre = p.Nombre, Precio = p.Precio, Id = p.Id });*/
+
+                if (proys != null && proys.Count() > 0)
+                    return View("Index", proys.ToList());
+                else
+                {
+                    TempData["ResultadoOperacion"] = "No hay productos disponibles";
+                    return View("Index", new List<ProyectoModel>());
+                }
+            }
+            else
+            {
+                TempData["ResultadoOperacion"] = "Error desconocido";
+                return View("Index");
+            }
+        }
+        //public ActionResult Details(int? id)
         //{
-        ////    RepositorioProyectos repoProyectos = new RepositorioProyectos();
-        //    string usu = (string)Session["usuario"];
-        //    if (repoProyectos.findPendiente(usu))
+        //    if (id == null)
         //    {
-        //         ViewBag.Mensaje = "Existe un proyecto pendiente, no se puede agregar otro";
-        //        return View();
-
-        //    }
-
-        //    //RepositorioConfiguraciones repoConfig = new RepositorioConfiguraciones();
-        //    //List<Cuota_Tasa> todasLasCuotasYTasas = repoConfig.CuotasyTasas();
-        //    //ViewBag.ListCuotas = todasLasCuotasYTasas;
-
-
-
-        //    return View();
-        //}
-
-        //// POST: Proyecto/Create
-        //[HttpPost]
-        //public ActionResult Index(string titulo, string descripcion, decimal monto,int cantidadCuotas, string tipoProyecto, int? cantidadIntegrantes, string experiencia, HttpPostedFileBase imagen)
-        //{
-        //    RepositorioProyectos repoProyectos = new RepositorioProyectos();          
-        //    try
-        //    {
-        //        string usu = (string)Session["usuario"];
-        //        RepositorioUsuarios repoUsuarios = new RepositorioUsuarios();
-        //        Solicitante u = (Solicitante)repoUsuarios.FindById(usu);
-        //        RepositorioConfiguraciones repoConfig = new RepositorioConfiguraciones();
-        //        //Cuota_Tasa tasaycuotas = repoConfig.FindTasaYcuoutas(cantidadCuotas);
-
-        //        if (tipoProyecto == "Cooperativo")
-        //        {
-        //            Cooperativo c = new Cooperativo();
-        //            if (c.SubirArchivoGuardarNombre(imagen)) {
-        //                c.titulo = titulo;
-        //                c.descripcion = descripcion;
-        //                c.monto = monto;
-        //                c.cuotas = cantidadCuotas;
-        //                //c.tasaInteres= tasaycuotas.tasa;
-        //                c.cantIntegrantes = (int)cantidadIntegrantes;
-        //                c.solicitante = u;
-        //                Guardar(c);
-        //                // Session["proyecto"] = c;
-        //                return RedirectToAction("Index", "Solicitante");
-
-        //            }                  
-        //        }
-        //        if (tipoProyecto == "Personal")
-        //        {
-        //            Personal c = new Personal();
-        //            if (c.SubirArchivoGuardarNombre(imagen))
-        //            {
-        //                c.titulo = titulo;
-        //                c.descripcion = descripcion;
-        //                c.monto = monto;
-        //                c.cuotas = cantidadCuotas;
-        //                //c.cuotas = tasaycuotas.cuotas;
-        //                //c.tasaInteres = tasaycuotas.tasa;
-        //                c.experiencia = experiencia;
-        //                c.solicitante = u;
-        //                Guardar(c);
-        //                //return  RedirectToAction("Confirmar", c);
-        //                return RedirectToAction("Index", "Solicitante");
-        //            }
-                  
-        //        }
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return RedirectToAction("Index");
-        //    }
-        //}
-
-        //[HttpPost]
-        //public ActionResult Confirmar(Proyecto p)
-        //{
-        //   //Proyecto p=(Proyecto) Session["proyecto"] ;
-        //    if (p.GetType().Name.ToUpper()=="COOPERATIVO") {
-        //        Cooperativo s=(Cooperativo)p;
-        //        return RedirectToAction("Guardar", s);
-        //    }
-
-        //        Personal pe = (Personal)p;          
-        //        return RedirectToAction("Guardar",pe);
-        //}
-
-        //[HttpPost]
-        //public ActionResult Guardar(Proyecto p)
-        //{
-        //    try
-        //    {
-        //        RepositorioProyectos repoProyectos = new RepositorioProyectos();
-
-        //        //Proyecto p = (Proyecto)Session["proyecto"];
-        //        string usu = (string)Session["usuario"];
-        //        RepositorioUsuarios repoUsuarios = new RepositorioUsuarios();
-        //        Solicitante u = (Solicitante)repoUsuarios.FindById(usu);
-        //        p.solicitante = u;
-        //        bool agregado = repoProyectos.Add(p);
-        //        if (agregado)
-        //        {
-        //            Session["proyecto"] = null;
-        //            return RedirectToAction("Index", "Solicitante");
-        //        }
-        //        Session["proyecto"] = null;
-        //        return RedirectToAction("Index", "Proyecto");
-        //    }
-        //    catch
-        //    {
-        //        RepositorioConfiguraciones repoConfig = new RepositorioConfiguraciones();
-        //        List<Cuota_Tasa> todasLasCuotasYTasas = repoConfig.CuotasyTasas();
-        //        ViewBag.ListCuotas = todasLasCuotasYTasas;
+        //        TempData["ResultadoOperacion"] = "Debe incluir un Id";
         //        return View("Index");
         //    }
-        //}
-
-        //public ActionResult Cancelar()
-        //{
-        //    Session["proyecto"] = null;
-        //    return View("index");
-        //}
-
-        //// GET: Proyecto/Edit/
-        //public ActionResult Edit(int id)
-        //{
-        //    RepositorioProyectos repositorioProyectos = new RepositorioProyectos();
-        //    Proyecto p = repositorioProyectos.FindById(id);
-                
-        //    return View(p);
-        //}
-
-        //// POST: Proyecto/Edit/5
-        //[HttpPost]
-        //public ActionResult Edit(Proyecto p, string comentarios)
-        //{
-        //    try
+        //    response = cliente.GetAsync(productoUri + "/" + id).Result;
+        //    if (response.IsSuccessStatusCode)
         //    {
-        //        RepositorioProyectos repo = new RepositorioProyectos();
-        //        Proyecto proy = repo.FindById(p.id);
-        //        p.solicitante = proy.solicitante;
-        //        bool actualizado= repo.Update(p, comentarios, (string)Session["usuario"]);
-        //        if (actualizado) {
-        //            return RedirectToAction("Index", "Admin");
-        //        }
-        //        return View(p);
+        //        var prod = response.Content.ReadAsAsync<Models.ProductoModel>().Result;
+        //        return View(prod);
         //    }
-        //    catch
+        //    else
         //    {
+        //        ViewBag.ResultadoOperacion = "No se puede encontrar el producto con el id " + id;
         //        return View();
         //    }
         //}
 
-        //// GET: Pryecto/Details/5
-
-        //public ActionResult Details(int id)
+        //public ActionResult GetByName(string name)
         //{
+        //    response = cliente.GetAsync(productoUri + "/getByName/" + name).Result;
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        var prod = response.Content.ReadAsAsync<Models.ProductoModel>().Result;
+        //        return View(prod);
+        //    }
+        //    else
+        //    {
+        //        ViewBag.ResultadoOperacion = "No se puede encontrar el producto con el nombre " + name;
+        //        return View();
+        //    }
+        //}
+        //public ActionResult Create()
+        //{
+
+
         //    return View();
         //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(ProductoModel prod)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+
+        //        //cliente.BaseAddress = productoUri;
+
+        //        var tareaPost = cliente.PostAsJsonAsync(productoUri, prod);
+
+        //        var result = tareaPost.Result;
+
+        //        if (result.IsSuccessStatusCode)
+        //        {
+        //            TempData["ResultadoOperacion"] = "Agregado el producto ";
+        //            return RedirectToAction("Index");
+        //        }
+        //        return View(prod);
+        //    }
+        //    else
+        //    {
+        //        TempData["ResultadoOperacion"] = "Ups! Verifique los datos";
+
+        //        return RedirectToAction("Index");
+        //    }
+        //}
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        ViewBag.ResultadoOperacion = "No te olvidaste de algo??";
+        //        return View();
+        //    }
+        //    response = cliente.GetAsync(productoUri + "/" + id.ToString()).Result;
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        var prod = response.Content.ReadAsAsync<Models.ProductoModel>().Result;
+        //        return View(prod);
+        //    }
+        //    else
+        //    {
+        //        ViewBag.ResultadoOperacion = "No se puede encontrar el producto con el id " + id;
+        //        return View();
+        //    }
+        //}
+        //[HttpPost]
+        //public ActionResult Edit(Models.ProductoModel prod)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var tareaPost = cliente.PutAsJsonAsync(productoUri + "/update/" + prod.Id, prod);
+
+        //        var result = tareaPost.Result;
+
+        //        if (result.IsSuccessStatusCode)
+        //        {
+        //            TempData["ResultadoOperacion"] = "Modificado";
+        //            return RedirectToAction("Index");
+        //        }
+        //        ViewBag.ResultadoOperación = "Ups! Verifique los datos";
+
+        //        return View(prod);
+        //    }
+        //    else
+        //    {
+        //        ViewBag.ResultadoOperación = "Ups! Verifique los datos";
+
+        //        return View(prod);
+        //    }
+        //}
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    response = cliente.GetAsync(productoUri + "/" + id.ToString()).Result;
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        var prod = response.Content.ReadAsAsync<Models.ProductoModel>().Result;
+        //        return View(prod);
+        //    }
+        //    else
+        //    {
+        //        ViewBag.ResultadoOperacion = "No se puede encontrar el producto con el id " + id;
+        //        return View();
+        //    }
+        //}
+
+        //// POST: ProductoModels/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    var tareaPost = cliente.DeleteAsync(productoUri + "/delete/" + id);
+
+        //    var result = tareaPost.Result;
+
+        //    if (result.StatusCode == HttpStatusCode.NoContent)
+        //    {
+        //        TempData["ResultadoOperacion"] = "El producto con el Id=" + id + " fue eliminado";
+        //        return RedirectToAction("Index");
+
+        //    }
+        //    TempData["ResultadoOperacion"] = "Ups! No se pudo eliminar";
+        //    return RedirectToAction("Index");
+        //}
+
+
 
     }
 }
+
+
---
+++ Obligatorio/Models/ProyectoModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Obligatorio.Models
+{
+    public class ProyectoModel
+
+    {
+        public string titulo { get; set; }
+        [Required]
+        public string descripcion { get; set; }
+        [Required]
+        public decimal monto { get; set; }
+        [Required]
+        public int cuotas { get; set; }
+        [Required]
+        public string rutaImagen { get; set; }
+        public string estado { get; set; } 
+        public DateTime fechaPresentacion { get; set; }
+        [Required]
+        public int puntaje { get; set; }
+    }
+}
