0997a1f3a49c074ec9e9cf776f9678280f439c17
RodrigoZambrana <sergiorodrigo2611@gmail.com>
2020-12-01 17:18:39 +0100

    Cambios vista de solicitante e inversor. Purebas de filtrado

--- Obligatorio/Controllers/InversorController.cs
+++ Obligatorio/Controllers/InversorController.cs
@@ -54,46 +54,7 @@ namespace Obligatorio.Controllers
             }
         }
 
-        public ActionResult Buscar(DateTime fechaini, DateTime fechaFin, string cedula, string titulo, string descripcion, string estado, decimal? monto)
-        {
-            string ruta = "";
-            string cedulaSession = (string)Session["usuario"];
-            string rol = (string)Session["rol"];
-            if (rol=="INVERSOR") {
-                 ruta = $"{proyectoUri}/busqueda/?fechaIni={fechaini}&fechaFin={fechaFin}&cedula={cedula}&titulo={titulo}&descripcion={descripcion}&estado={estado}&monto={monto}";
-                Uri uri = new Uri(ruta);
-
-                var response = cliente.GetAsync(uri).Result;
-                if (response.IsSuccessStatusCode)
-                {
-                    var lista = response.Content.ReadAsAsync<IEnumerable<ProyectoModel>>().Result;
-                    ViewBag.Mensaje = $"Se encontraron {lista.Count()} resultados";
-                    View("Index", "Inversor", lista);
-                }
-                ViewBag.Mensaje = "No se encontraron resultados";
-                return View();
-            }
-            if (rol == "SOLICITANTE")
-            {
-                ruta = $"{proyectoUri}/busqueda/?fechaIni={fechaini}&fechaFin={fechaFin}&cedula={cedulaSession}&titulo={titulo}&descripcion={descripcion}&estado={estado}&monto={monto}";
-
-                Uri uri = new Uri(ruta);
-
-                var response = cliente.GetAsync(uri).Result;
-                if (response.IsSuccessStatusCode)
-                {
-                    var lista = response.Content.ReadAsAsync<IEnumerable<ProyectoModel>>().Result;
-                    ViewBag.Mensaje = $"Se encontraron {lista.Count()} resultados";
-                    return View("Index","Solicitante",lista);
-                }
-                ViewBag.Mensaje = "No se encontraron resultados";
-                return View();
-            }
-            return View("Index","Home");
-        }
-
-
-
+      
         //    // GET: Inversors/Details/5
         //    public ActionResult Details(string id)
         //    {
