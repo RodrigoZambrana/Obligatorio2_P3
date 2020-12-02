95487cd7ef4a7b9ed47caf4ccfe400554b19b598
RodrigoZambrana <sergiorodrigo2611@gmail.com>
2020-12-02 17:37:30 +0100

    Se agrega model de inversior y reposittio de inversiores

---
+++ WebApiObligatorio2/Models/InvertirModel.cs
@@ -0,0 +1,17 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApiObligatorio2.Models
+{
+    public class InvertirModel
+    {
+        public Solicitante solicitante { get; set; }
+        public Proyecto poryecto { get; set; }
+        public decimal montoSolicitadoConIntereses { get; set; }
+        public int montoCuotaIntereses { get; set; }
+    }
+}
